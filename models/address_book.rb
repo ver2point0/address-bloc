c935560786403839e172411a55daadd5ea3c111a
John McIntosh II <jmci10@hotmail.com>
2015-09-22 19:57:00 +0000

    Assignment 19 complete update

--- models/address_book.rb
+++ models/address_book.rb
@@ -20,14 +20,12 @@ class AddressBook
     end
     
     def remove_entry(name, phone_number, email)
-      delete_entry = nil
-      
       @entries.each do |entry|
           if (name == entry.name && phone_number == entry.phone_number && email = entry.email)
-            delete_entry = entry
+            @entries.delete(entry)
+            break
           end
       end
-      @entries.delete(delete_entry)
     end
     
 end
